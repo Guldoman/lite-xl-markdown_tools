f65e5760637d950201275debe05a512ffb7b4de5
Guldoman <giulio.lettieri@gmail.com>
2025-03-27 15:05:39 +0100

    feat: add automatic and on-demand table formatting

--- init.lua
+++ init.lua
@@ -86,4 +86,90 @@ keymap.add {
 	["ctrl+i"] = "markdown-tools:italicize",
 }
 
+-- TODO: command to convert to/from csv
+
+---@type Table
+local Table = require "plugins.markdown_tools.table"
+
+local function is_view_supported_and_in_table()
+	local res, dv = MarkdownTools.is_view_supported()
+	if not res or not dv then return false end
+	local doc = dv.doc
+	for _, line1, _, line2 in doc:get_selections(true, true) do
+		-- TODO: maybe limit number of lines to check
+		-- Check first and last line, then every three lines, as a table has at least 3 rows
+		local t_loc = Table.is_table(doc.lines, line1)
+		if t_loc then return true, dv end
+		t_loc = Table.is_table(doc.lines, line2)
+		if t_loc then return true, dv end
+		for i=line2-1,line1+1,-3 do
+			t_loc = Table.is_table(doc.lines, i)
+			if t_loc then return true, dv end
+		end
+	end
+	return false
+end
+
+local function format_if_needed(doc)
+	-- TODO: don't apply this to non-markdown files
+	local tables = { }
+	local checked_lines = { }
+	local function format_line(line)
+		if checked_lines[line] then
+				return
+		end
+		checked_lines[line] = true
+		local t_loc = Table.is_table(doc.lines, line)
+		if not t_loc then
+			return
+		end
+		for i=t_loc.line1,t_loc.line2 do
+			checked_lines[i] = true
+		end
+		local t_info = Table.get_table_info(doc.lines, t_loc)
+		local t_format = Table.build_table_format(t_info)
+		table.insert(tables, t_format)
+	end
+	for _, line1, _, line2 in doc:get_selections(true, true) do
+		-- TODO: maybe limit number of lines to check
+		-- Check first and last line, then every three lines, as a table has at least 3 rows
+		format_line(line1)
+		format_line(line2)
+		for line=line2-1,line1+1,-3 do
+			format_line(line)
+		end
+	end
+	for _, t_format in ipairs(tables) do
+		Table.apply_table_format(doc, t_format)
+	end
+end
+
+command.add(is_view_supported_and_in_table, {
+	["markdown-tools:format-table"] = function(dv)
+		format_if_needed(dv.doc)
+	end
+})
+
+local Doc = require "core.doc"
+local doc_text_input = Doc.text_input
+function Doc:text_input(...)
+	local res = { doc_text_input(self, ...) }
+	format_if_needed(self)
+	return table.unpack(res)
+end
+
+local doc_backspace = command.map["doc:backspace"].perform
+command.map["doc:backspace"].perform = function(dv, ...)
+	local res = { doc_backspace(dv, ...) }
+	format_if_needed(dv.doc)
+	return table.unpack(res)
+end
+
+local doc_delete = command.map["doc:delete"].perform
+command.map["doc:delete"].perform = function(dv, ...)
+	local res = { doc_delete(dv, ...) }
+	format_if_needed(dv.doc)
+	return table.unpack(res)
+end
+
 return MarkdownTools
