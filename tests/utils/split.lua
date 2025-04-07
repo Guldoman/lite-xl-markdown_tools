a6b284e6896d891a49f35897bdd67d01867cea8e
Guldoman <giulio.lettieri@gmail.com>
2025-04-07 04:09:54 +0200

    feat: add support for escapes in `Utils.split`
    Also allow multi-character tokens.

--- tests/utils/split.lua
+++ tests/utils/split.lua
@@ -38,4 +38,139 @@ split_tests:add_test("Split with no final value", function()
 	return true
 end)
 
+split_tests:add_test("Split with escape", function()
+	local text = {
+		"Hello world\\",
+		"How are you doing",
+	}
+	local result, token_indexes = Utils.split(table.concat(text, "\n"), "\n", "\\")
+	assert(#result == 1, #result)
+	assert(result[1] == table.concat(text, "\n"), result[1])
+	assert(#token_indexes == 0)
+	return true
+end)
+
+split_tests:add_test("Split with multiple escapes", function()
+	local text = {
+		"\\",
+		"Hello world",
+		"How are you doing\\",
+		"\\",
+	}
+	local result, token_indexes = Utils.split(table.concat(text, "\n"), "\n", "\\")
+	assert(#result == 2, #result)
+	assert(result[1] == table.concat({ text[1], text[2] }, "\n"), result[1])
+	assert(result[2] == table.concat({ text[3], text[4] }, "\n"), result[2])
+	assert(#token_indexes == 1)
+	assert(token_indexes[1] == #text[1] + #text[2] + 2, token_indexes[1])
+	return true
+end)
+
+split_tests:add_test("Split with every token escaped", function()
+	local text = {
+		"\\",
+		"Hello world\\",
+		"How are you doing\\",
+		"\\",
+	}
+	local result, token_indexes = Utils.split(table.concat(text, "\n"), "\n", "\\")
+	assert(#result == 1, #result)
+	assert(result[1] == table.concat(text, "\n"), result[1])
+	assert(#token_indexes == 0)
+	return true
+end)
+
+split_tests:add_test("Split with irrelevant escapes", function()
+	local text = {
+		"\\",
+		"\\Hello\\ world\\",
+		"\\How are\\ you doing\\",
+		"\\",
+	}
+	local result, token_indexes = Utils.split(table.concat(text, "\n"), "\n", "\\")
+	assert(#result == 1, #result)
+	assert(result[1] == table.concat(text, "\n"), result[1])
+	assert(#token_indexes == 0)
+	return true
+end)
+
+split_tests:add_test("Split with only escapes and tokens", function()
+	local text = {
+		"\\",
+		"\\\\\\",
+		"\\\\\\",
+		"\\",
+	}
+	local result, token_indexes = Utils.split(table.concat(text, "\n"), "\n", "\\")
+	assert(#result == 1, #result)
+	assert(result[1] == table.concat(text, "\n"), result[1])
+	assert(#token_indexes == 0)
+	return true
+end)
+
+split_tests:add_test("Split with only escapes", function()
+	local text = {
+		"\\\\\\\\\\\\\\\\",
+	}
+	local result, token_indexes = Utils.split(table.concat(text, "\n"), "\n", "\\")
+	assert(#result == 1, #result)
+	assert(result[1] == table.concat(text, "\n"), result[1])
+	assert(#token_indexes == 0)
+	return true
+end)
+
+split_tests:add_test("Split with multi-characters token", function()
+	local text = {
+		"Hello world",
+		"How are you doing"
+	}
+	local result, token_indexes = Utils.split(table.concat(text, "\n"), " w")
+	assert(#result == 2)
+	assert(result[1] == "Hello", result[1])
+	assert(result[2] == "orld\nHow are you doing")
+	assert(#token_indexes == 1)
+	assert(token_indexes[1] == 6, token_indexes[1])
+	return true
+end)
+
+split_tests:add_test("Split with multi-characters escape", function()
+	local text = {
+		"Hello@#",
+		"world",
+		"How@#",
+		"are",
+		"you@#",
+		"doing",
+	}
+	local result, token_indexes = Utils.split(table.concat(text, "\n"), "\n", "@#")
+	assert(#result == 3, #result)
+	assert(result[1] == "Hello@#\nworld", result[1])
+	assert(result[2] == "How@#\nare", result[2])
+	assert(result[3] == "you@#\ndoing", result[3])
+	assert(#token_indexes == 2)
+	assert(token_indexes[1] == 14, token_indexes[1])
+	assert(token_indexes[2] == 24, token_indexes[2])
+	return true
+end)
+
+split_tests:add_test("Split with multi-characters escape with same character", function()
+	local text = {
+		"Hello--",
+		"world",
+		"How--",
+		"are",
+		"you--",
+		"doing",
+	}
+	local result, token_indexes = Utils.split(table.concat(text, "\n"), "\n", "--")
+	assert(#result == 3, #result)
+	assert(result[1] == "Hello--\nworld", result[1])
+	assert(result[2] == "How--\nare", result[2])
+	assert(result[3] == "you--\ndoing", result[3])
+	assert(#token_indexes == 2)
+	assert(token_indexes[1] == 14, token_indexes[1])
+	assert(token_indexes[2] == 24, token_indexes[2])
+	return true
+end)
+
 return split_tests
--- utils.lua
+++ utils.lua
@@ -1,17 +1,38 @@
 local Utils = { }
 
-function Utils.split(str, token)
+function Utils.split(str, token, escape_token)
+	local escape_token_len = escape_token and #escape_token
 	local res = { }
 	local indexes = { }
 	local index = 1
 	repeat
-		local initial, final = string.find(str, token, index, true)
+		local initial, final
+		local tmp_index = index
+		repeat
+			local done = false
+			initial, final = string.find(str, token, tmp_index, true)
+			if not initial or not escape_token or escape_token_len == 0 then
+				-- No token to the end of the string
+				break
+			end
+			-- Check for the number of escapes
+			local n = -1
+			repeat
+				n = n + 1
+				local idx = final - ((n + 1) * escape_token_len)
+			until string.sub(str, idx, idx + escape_token_len - 1) ~= escape_token
+			if n % 2 ~= 0 then
+				tmp_index = final + 1
+			else
+				done = true
+			end
+		until done
 		if not initial then
 			final = #str + 1
 		else
-			table.insert(indexes, final)
+			table.insert(indexes, initial)
 		end
-		table.insert(res, string.sub(str, index, final - 1))
+		table.insert(res, string.sub(str, index, (initial or final) - 1))
 		index = final + 1 --[[@as integer]]
 	until not initial
 	return res, indexes
